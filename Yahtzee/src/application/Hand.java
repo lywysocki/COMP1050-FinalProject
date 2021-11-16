25d1528697be1056746a6c46516422e9a67f9cc4
Madi Reitzell <reitzellm@PF2YN14S.wit.private>
2021-11-15 22:37:04 -0500

    Hand and Scorecard Classes

--- Yahtzee/src/application/Hand.java
+++ Yahtzee/src/application/Hand.java
@@ -24,9 +24,51 @@ public class Hand extends Dice{
 		return value;
 	}
 	
+	@Override
 	public String toString() {
 		return String.format("D1: %s, D2: %s, D3: %s, D4: %s, D5: %s %n", a[0].getValue(), a[1].getValue(), a[2].getValue(), a[3].getValue(), a[4].getValue());
 	}
 	
+	
+	//takes in getHandValue and either 3, 4, or 5 for k to see if the hand fits 3 of a kind, 4 of a kind, or Yahtzee
+	public static boolean hasAmountOf(int[] values, int k) {
+		boolean isOfAKind=false;
+		int i=0;
+		int count=1;
+		if (k<1 || values.length==0) {
+			return isOfAKind;
+		}
+		else if (k>values.length) {
+			return isOfAKind;
+		}
+		else if(k==1) {
+			isOfAKind=true;
+			return isOfAKind;
+		}
+		else {			
+			while (i+1<values.length) {
+				count = (values[i+1]==values[i]) ? count+1:1;
+				if (count==k || count>k) {
+					isOfAKind=true;
+					return isOfAKind;
+					}
+				i++;
+			}	
+		}
+		return isOfAKind;
+	
+	}
+	
+	public static boolean isFullHouse(int[] v) {
+		int value1 = v[0];
+		int value2 = v[1];
+		for (int i=0; i<v.length; i++) {
+			if (value1 != v[i] || value2 != v[i] ) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
 	
 }
---
+++ Yahtzee/src/application/ScoreCard.java
@@ -0,0 +1,26 @@
+package application;
+
+public class ScoreCard {
+	
+	/**
+	 * Two arrays for both sections of score card: 
+	 * first section is the points for each number (ones, twos, etc.)
+	 * second section for "special" hands (three of a kind, full house etc.)
+	 * 
+	*/
+	public ScoreCard() {
+		int[] upper = {0, 0, 0, 0, 0, 0};
+		// upper[0]=ones, upper[1]=twos, upper[2]=threes, upper[3]=fours, upper[4]=fives], upper[5]=sixes
+		int[] lower = {0, 0, 0, 0, 0};
+		// lower[0]=three of a kind, lower[1]=four of a kind, lower[2]= small straight
+		// lower[3]=large straight, lower[4]=chance
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
