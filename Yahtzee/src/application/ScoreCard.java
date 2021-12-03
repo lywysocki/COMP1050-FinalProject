8393e75bc99685e7058680454139b2e739514220
reitzellm <reitzellm@PF2YN14S.wit.private>
2021-12-03 09:58:51 -0500

    Methods to set each score and get total score

--- Yahtzee/src/application/ScoreCard.java
+++ Yahtzee/src/application/ScoreCard.java
@@ -2,24 +2,75 @@ package application;
 
 public class ScoreCard {
 	
-	/**
-	 * Two arrays for both sections of score card: 
-	 * first section is the points for each number (ones, twos, etc.)
-	 * second section for "special" hands (three of a kind, full house etc.)
-	 * 
-	*/
+	int[] upper = new int[6];
+	// upper[0]=ones, upper[1]=twos, upper[2]=threes, upper[3]=fours, upper[4]=fives], upper[5]=sixes
+	int[] lower = new int[6];
+	// lower[0]=three of a kind, lower[1]=four of a kind, lower[2]= full house, lower[3]= small straight
+	// lower[4]=large straight, lower[5]=chance
+	
+	
 	public ScoreCard() {
-		int[] upper = {0, 0, 0, 0, 0, 0};
-		// upper[0]=ones, upper[1]=twos, upper[2]=threes, upper[3]=fours, upper[4]=fives], upper[5]=sixes
-		int[] lower = {0, 0, 0, 0, 0};
-		// lower[0]=three of a kind, lower[1]=four of a kind, lower[2]= small straight
-		// lower[3]=large straight, lower[4]=chance
-		
-		
+		for (int i=0; i<6; i++) {
+			upper[i]=0;
+			lower[i]=0;
+		}
 	}
 	
+	public int getTotalScore() {
+		int total = 0;
+		for (int i=0; i<6; i++) {
+			total+=upper[i]+lower[i];
+		}
+		return total;
+	}
 	
+	public void setOnes(int amt) {
+		upper[0]=amt;
+	}
 	
+	public void setTwos(int amt) {
+		upper[1]=amt;
+	}
+	
+	public void setThrees(int amt) {
+		upper[2]=amt;
+	}
+	
+	public void seFours(int amt) {
+		upper[3]=amt;
+	}
+	
+	public void setFives(int amt) {
+		upper[4]=amt;
+	}
+	
+	public void setSixes(int amt) {
+		upper[5]=amt;
+	}
+	
+	public void setThreeOfAKind(int total) {
+		lower[0]=total;
+	}
+	
+	public void setFourOfAKind(int total) {
+		lower[1]=total;
+	}
+	
+	public void setFullHouse() {
+		lower[2]=25;
+	}
+	
+	public void setSmallStraight() {
+		lower[3]= 30;
+	}
+	
+	public void setLargeStraight() {
+		lower[4]=40;
+	}
+	
+	public void setChance(int total) {
+		lower[5]=total;
+	}
 	
 	
 	
