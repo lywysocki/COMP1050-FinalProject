d4c73b16c306754fcc479e82c4b4d42d5598ac5d
reitzellm <reitzellm@PF2YN14S.wit.private>
2021-12-09 02:07:11 -0500

    playAgain goes back to launch page

--- Yahtzee/src/application/EndController.java
+++ Yahtzee/src/application/EndController.java
@@ -1,8 +1,14 @@
 package application;
+import java.io.IOException;
 import java.util.ArrayList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class EndController {
 
@@ -30,8 +36,12 @@ public class EndController {
     
     
     @FXML
-    void playAgain(ActionEvent event) {
-
+    void playAgain(ActionEvent event) throws IOException {
+    	Parent launchParent = FXMLLoader.load(getClass().getResource("Launch.fxml"));
+		Scene s = new Scene(launchParent);
+		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+		window.setScene(s);
+		window.show();
     }
 
 }
